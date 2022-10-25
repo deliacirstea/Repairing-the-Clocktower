66ba54eec4b51f33530624845698e574bc86a96b
deliacirstea <deliutacirstea@gmail.com>
2022-10-25 11:49:32 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.Title = " Repairing the Clocktower";
+Console.WriteLine("Help us fixing the clock in the Consola town by choosing a number:");
+
+
+string input = Console.ReadLine();
+int number = Convert.ToInt32(input);
+
+if (number == 7)
+    Console.WriteLine("Tick");
+if (number != 7)
+    Console.WriteLine("Tock");
